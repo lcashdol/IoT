9a7dbf153a62c7da0f6a3a4fab583c14e8fc07cd
Larry W. Cashdollar <larry0@me.com>
2017-12-04 14:38:10 -0500

    Update detux.sh

--- detux.sh
+++ detux.sh
@@ -2,23 +2,25 @@
 
 API_KEY=`cat ~/api_key`
 
-
 if [ -z $2 ]; then
-echo "detux.org: API Client Interface v1.0"
-echo "Usage $0 SHA|file|search string report|search|submit";
-exit
+  echo "detux.org: API Client Interface v1.0"
+  echo "Usage $0 SHA|file|search string report|search|submit";
+  exit;
 fi
 
 echo "Using API $API_KEY";
+
 if [ $2 = "report" ]; then
-echo "SHA256: $1";
-curl --request POST 'https://detux.org/api/report.php' --data "api_key=$API_KEY&sha256=$1"
+  echo "SHA256: $1";
+  curl --request POST 'https://detux.org/api/report.php' --data "api_key=$API_KEY&sha256=$1"
 fi
+
 if [ $2 = "search" ]; then
-echo "Search: $1";
-curl --request POST 'https://detux.org/api/search.php' --data "api_key=$API_KEY&search=$1"
+  echo "Search: $1";
+  curl --request POST 'https://detux.org/api/search.php' --data "api_key=$API_KEY&search=$1"
 fi
+
 if [ $2 = "submit" ]; then
-echo "File: $1";
-curl --request POST 'https://detux.org/api/submit.php' --data "api_key=$API_KEY&file=$1=&private=0"
+  echo "File: $1";
+  curl --request POST 'https://detux.org/api/submit.php' --data "api_key=$API_KEY&file=$1=&private=0"
 fi
